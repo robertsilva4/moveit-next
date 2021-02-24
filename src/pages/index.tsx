a4595640232b09390e6c0285060cb4e5fb815788
Robert Silva <robert.silva4@fatec.sp.gov.br>
2021-02-24 00:07:37 -0300

    altered structure of this project

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { ExperienceBar } from "../components/ExperienceBar";
+
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar/>
+    </div>
+  )
+}
